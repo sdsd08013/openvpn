0c7b34d19846a05030b62f155b911273c22974fb
sdsd08013 <sd08013+git@gmail.com>
2022-12-01 23:24:19 +0900

    save config to clitn_crt

--- make_config.sh
+++ make_config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-KEY_DIR=./ca
+KEY_DIR=./client
 SERVER_DIR=./server
 OUTPUT_DIR=./client
 BASE_CONFIG=./client.conf
 
 cat ${BASE_CONFIG} <(echo -e '<ca>') ${KEY_DIR}/ca.crt <(echo -e '</ca>\n<cert>') ${SERVER_DIR}/${1}.crt <(echo -e '</cert>\n<key>') ${SERVER_DIR}/${1}.key <(echo -e '</key>\n<tls-crypt>') ${KEY_DIR}/ta.key <(echo -e '</tls-crypt>') > ${OUTPUT_DIR}/${2}.ovpn
+
+cp ${OUTPUT_DIR}/${2}.ovpn ./client_crt
--- setup_crt.sh
+++ setup_crt.sh
@@ -7,8 +7,8 @@ cd ./easy-rsa-3.0.1/easyrsa3
 openvpn --genkey --secret ta.key
 ./easyrsa gen-req $1
 ./easyrsa sign-req $2 $1
-cp ./pki/dh.pem ../../ca
-cp ./pki/ca.crt ../../ca
-cp ./ta.key ../../ca
+cp ./pki/dh.pem ../../client
+cp ./pki/ca.crt ../../client
+cp ./ta.key ../../client
 cp ./pki/issued/sky-vpn.crt ../../server
 cp ./pki/private/sky-vpn.key ../../server
