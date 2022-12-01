6c9d0575829231143782499bf07630c1b4f5d96a
sdsd08013 <sd08013+git@gmail.com>
2022-12-02 04:19:05 +0900

    make  crt dir

--- setup_crt.sh
+++ setup_crt.sh
@@ -7,8 +7,8 @@ cd ./easy-rsa-3.0.1/easyrsa3
 openvpn --genkey --secret ta.key
 ./easyrsa gen-req $1
 ./easyrsa sign-req $2 $1
-cp ./pki/dh.pem ../../client
-cp ./pki/ca.crt ../../client
-cp ./ta.key ../../client
-cp ./pki/issued/sky-vpn.crt ../../server
-cp ./pki/private/sky-vpn.key ../../server
+cp ./pki/dh.pem ../../crt/ca
+cp ./pki/ca.crt ../../crt/ca
+cp ./ta.key ../../crt/ca
+cp ./pki/issued/sky-vpn.crt ../../crt/server
+cp ./pki/private/sky-vpn.key ../../crt/server
