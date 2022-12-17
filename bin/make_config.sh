3f469add506be27c1846a11d16a65c17ec8b2a2b
sdsd08013 <sd08013+git@gmail.com>
2022-12-17 20:46:55 +0900

    minor fix

--- bin/make_config.sh
+++ bin/make_config.sh
@@ -2,7 +2,7 @@
 
 KEY_DIR=./crt/ca
 CLIENT_DIR=./crt/client
-OUTPUT_DIR=./crt/client
+OUTPUT_DIR=./crt/ovpn
 BASE_CONFIG=./client.conf
 
 cat ${BASE_CONFIG} <(echo -e '<ca>') ${KEY_DIR}/ca.crt <(echo -e '</ca>\n<cert>') ${CLIENT_DIR}/${1}.crt <(echo -e '</cert>\n<key>') ${CLIENT_DIR}/${1}.key <(echo -e '</key>\nkey-direction 1\n<tls-auth>') ${KEY_DIR}/ta.key <(echo -e '</tls-auth>') > ${OUTPUT_DIR}/${2}.ovpn
